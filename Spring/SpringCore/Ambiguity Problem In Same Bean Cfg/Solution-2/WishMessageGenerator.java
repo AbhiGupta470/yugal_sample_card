f9ba07665783febce69c811ddd302df006d3417e
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2025-06-02 13:47:18 +0530

    Create WishMessageGenerator.java

---
+++ Spring/SpringCore/Ambiguity Problem In Same Bean Cfg/Solution-2/WishMessageGenerator.java
@@ -0,0 +1,28 @@
+package com.nt.sbeans;
+
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("wmg")
+public class WishMessageGenerator {
+	// HAS-A properties - Field Injection
+	@Autowired
+	private LocalDateTime dateTime;
+	
+	
+	public String generateWishMessage(String user)
+	{
+		int hour =  dateTime.getHour();
+		if(hour >=7 && hour < 12)
+			return "Good morning: " + user;
+		else if(hour >=12 && hour <16)
+			return "Good afternoon: " + user;
+		else if(hour >=16 && hour < 20)
+			return "Good evening: " + user;
+		else
+			return "Good night: " + user;
+	}
+}
